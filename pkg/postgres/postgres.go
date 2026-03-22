02fd0a185098d34d4146024d53f972261d7dafc3
der-johanni <ivangorbanwork@gmail.com>
2026-03-22 11:14:34 +0300

    feat: ping

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -21,6 +21,17 @@ func (cfg *Config) ToDSN() string {
 	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
 }
 
+func Ping(db *sql.DB) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return fmt.Errorf("postgres ping: %w", err)
+	}
+
+	return nil
+}
+
 func New(ctx context.Context, cfg Config) (*sql.DB, error) {
 	dsn := cfg.ToDSN()
 	db, err := sql.Open("pgx", dsn)
