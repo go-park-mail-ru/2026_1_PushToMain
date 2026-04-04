44ec314098c286b6ab304e3972f1e4cd518eea08
johanngorban <ivangorbanwork@gmail.com>
2026-04-04 13:34:01 +0300

    feat: auth repo

--- internal/app/user/repository/user.go
+++ internal/app/user/repository/user.go
@@ -2,39 +2,89 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/go-park-mail-ru/2026_1_PushToMain/internal/app/user/models"
 )
 
 var ErrUserNotFound = errors.New("user not found")
+var ErrUserDbNotInited = errors.New("user database is not inited")
 
 type Repository struct {
-	mu    sync.Mutex
-	users map[string]models.User
+	mu    	sync.Mutex
+	userDb 	*sql.DB
 }
 
-func New() *Repository {
+func New(userDb *sql.DB) *Repository {
 	return &Repository{
-		users: make(map[string]models.User),
+		userDb: userDb,
 	}
 }
 
 func (repo *Repository) Save(ctx context.Context, user models.User) (int64, error) {
+	query := `
+		INSERT INTO users (email, password_hash, name, surname, image_path)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING id
+	`
+
+	if repo.userDb == nil {
+		return 0, ErrUserDbNotInited
+	}
+
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
-	user.ID = 1 //todo пока не прикрутили реалую бд просто id 1 будет выдавать
-	repo.users[user.Email] = user
-	return user.ID, nil
+
+	var userId int64
+	err := repo.userDb.QueryRowContext(
+		ctx,
+		query,
+		user.Email,
+		user.Password,
+		user.Name,
+		user.Surname,
+		user.ImagePath,
+	).Scan(&userId)
+
+	if err != nil {
+		return 0, fmt.Errorf("Failed to save user: %w", err)
+	}
+
+	return userId, nil
 }
 
 func (repo *Repository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	if repo.userDb == nil {
+		return nil, ErrUserDbNotInited
+	}
+
 	repo.mu.Lock()
 	defer repo.mu.Unlock()
-	user, ok := repo.users[email]
-	if !ok {
+
+	query := fmt.Sprintf(`
+		SELECT id, password_hash, name, surname, image_path
+		FROM users
+		WHERE email == %s
+	`, email)
+
+	row := repo.userDb.QueryRowContext(ctx, query)
+	if row.Err() != nil {
+		return nil, row.Err()
+	}
+
+	user := models.User{
+		Email: email,
+	}
+
+	err := row.Scan(&user.ID, &user.Password, &user.Name, &user.Surname, &user.ImagePath)
+	if err == sql.ErrNoRows {
 		return nil, ErrUserNotFound
+	}
+	if err != nil {
+		return  nil, err
 	}
 
 	return &user, nil
