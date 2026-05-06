51a1f878e31e61ae3cba8f2d858785d947d200f1
johanngorban <ivangorbanwork@gmail.com>
2026-05-06 12:26:33 +0300

    docs: help added

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -19,17 +19,20 @@ func New(serviceName string, serviceExtraCollectors ...prometheus.Collector) *Me
 	errors := prometheus.NewCounter(prometheus.CounterOpts{
 		Name:        "errors_total",
 		ConstLabels: prometheus.Labels{"service": serviceName},
+		Help:        "Total number of errors",
 	})
 
 	hits := prometheus.NewCounter(prometheus.CounterOpts{
 		Name:        "hits_total",
 		ConstLabels: prometheus.Labels{"service": serviceName},
+		Help:        "Total number of hits",
 	})
 
 	timing := prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:        "timing_seconds",
 		ConstLabels: prometheus.Labels{"service": serviceName},
 		Buckets:     prometheus.ExponentialBucketsRange(0.005, 10, 10),
+		Help:        "Total timings",
 	})
 
 	toRegister := []prometheus.Collector{
