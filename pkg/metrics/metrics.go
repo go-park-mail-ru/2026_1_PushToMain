ac60a5d716ad8cb7cc8e649dbb616c75ae0a5c3b
johanngorban <ivangorbanwork@gmail.com>
2026-05-06 13:14:28 +0300

    feat: metrics refacotred and upgraded

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,55 +1,80 @@
 package metrics
 
 import (
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type Metrics struct {
-	reg    *prometheus.Registry
-	Errors *prometheus.CounterVec
-	Hits   *prometheus.CounterVec
-	Timing *prometheus.HistogramVec
+	reg            *prometheus.Registry
+	requestsTotal  *prometheus.CounterVec
+	requeustTiming *prometheus.HistogramVec
 	// CPU, RAM and Go stats are not there
 }
 
-func New(serviceName string, serviceExtraCollectors ...prometheus.Collector) *Metrics {
+func New(serviceName string, namespace string, serviceExtraCollectors ...prometheus.Collector) *Metrics {
 	reg := prometheus.NewRegistry()
+	constLabels := prometheus.Labels{"service": serviceName}
 
-	errors := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name:        "errors_total",
-		ConstLabels: prometheus.Labels{"service": serviceName},
-		Help:        "Total number of errors",
-	}, []string{"path", "status"})
+	requestTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace:   namespace,
+			Name:        "http_requests_total",
+			Help:        "Total number of HTTP requests",
+			ConstLabels: constLabels,
+		},
+		[]string{"code", "method", "path"},
+	)
 
-	hits := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name:        "hits_total",
-		ConstLabels: prometheus.Labels{"service": serviceName},
-		Help:        "Total number of hits",
-	}, []string{"path", "status"})
-
-	timing := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:        "timing_seconds",
-		ConstLabels: prometheus.Labels{"service": serviceName},
-		Buckets:     prometheus.ExponentialBucketsRange(0.005, 10, 10),
-		Help:        "Total timings",
-	}, []string{"path", "status"})
+	requestTiming := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace:   namespace,
+			Name:        "http_requests_timing_seconds",
+			Help:        "Timing of HTTP requests in seconds",
+			ConstLabels: constLabels,
+		},
+		[]string{"method", "path"},
+	)
 
 	toRegister := []prometheus.Collector{
 		collectors.NewGoCollector(),                                       // Go stuff
 		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}), // CPU
-		errors,
-		hits,
-		timing,
+		requestTiming,
+		requestTotal,
 	}
 	toRegister = append(toRegister, serviceExtraCollectors...)
-
 	reg.MustRegister(toRegister...)
 
 	return &Metrics{
-		reg:    reg,
-		Errors: errors,
-		Hits:   hits,
-		Timing: timing,
+		reg:            reg,
+		requestsTotal:  requestTotal,
+		requeustTiming: requestTiming,
 	}
+}
+
+func (m *Metrics) IncRequest(statusCode int, method string, path string) {
+	m.requestsTotal.With(prometheus.Labels{
+		"code":   strconv.Itoa(statusCode),
+		"method": method,
+		"path":   path,
+	}).Inc()
+}
+
+func (m *Metrics) ObserveDuration(method string, path string, duration time.Duration) {
+	m.requeustTiming.With(prometheus.Labels{
+		"method": method,
+		"path":   path,
+	}).Observe(duration.Seconds())
+}
+
+func (m *Metrics) Handler() http.Handler {
+	return promhttp.HandlerFor(
+		m.reg,
+		promhttp.HandlerOpts{Registry: m.reg},
+	)
 }
