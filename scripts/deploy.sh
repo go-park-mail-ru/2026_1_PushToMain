4627ff9836534b781e8be7cdb6ffa1f505b59df3
johanngorban <ivangorbanwork@gmail.com>
2026-05-15 21:22:37 +0300

    fix: deployment flow changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e
-
 cd ~/backend
 
-rm -f .env
-echo "POSTGRES_USER=$1" >> .env
-echo "POSTGRES_PASSWORD=$2" >> .env
-echo "POSTGRES_DBNAME=$3" >> .env
-echo "MINIO_ACCESS_KEY=$4" >> .env
-echo "MINIO_SECRET_KEY=$5" >> .env
-echo "JWT_SECRET=$6" >> .env
-echo "GRAFANA_PASSWORD=$7" >> .env
+echo "$2" | docker login ghcr.io -u "$1" --password-stdin
 
-sudo docker compose -f docker-compose.prod.yml pull
-sudo docker compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --remove-orphans
+docker image prune -f
